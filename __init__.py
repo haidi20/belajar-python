fd0f8f0fa4a3bef7426c3ddbe692c9fd77c72cd7
haidi <haidinurhadinata@gmail.com>
2018-11-06 09:19:26 +0700

    update-login

--- __init__.py
+++ __init__.py
@@ -31,14 +31,21 @@ def about():
 @app.route('/register', methods=['GET', 'POST'])
 def register():
 	form = RegistrationForm()
-	if form.validate_on_submit():
-		flash(f'Account created for {form.username.data}!', 'success')
+	if form.validate_on_submit(): 
+		flash('Account created for {form.username.data}!', 'success')
 		return redirect(url_for('home'))
 	return render_template('register.html', title='register', form=form)
 
-@app.route('/login')
+@app.route('/login', methods=['GET', 'POST'])
 def login():
 	form = LoginForm()
+	if form.validate_on_submit():
+		if form.email.data == 'nata@nata.com' and form.password.data == 'samarinda':
+			flash('You have been logged in!', 'success')
+			return redirect(url_for('home'))
+		else:
+			flash('Login Unsuccessful. Please check username and password', 'danger')
+
 	return render_template('login.html', title='login', form=form)
 	
 
